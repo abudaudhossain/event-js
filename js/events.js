2333ada35883af4d88ece66096ab40dc540ada20
abudaudhossain <abudaudhossainsumon@gmail.com>
2021-08-13 00:20:01 +0600

    add eventListener

---
+++ js/events.js
@@ -0,0 +1,21 @@
+function makeRed() {
+    document.body.style.backgroundColor = 'red';
+}
+
+const makeGreen = document.getElementById('make-green');
+makeGreen.onclick = getMakeGreen;
+
+function getMakeGreen() {
+    document.body.style.backgroundColor = 'green';
+}
+
+const makeBlue = document.getElementById('make-blue');
+makeBlue.onclick = function () {
+    document.body.style.backgroundColor = 'blue';
+}
+
+const makeBlueViolet = document.getElementById('make-blue-violet');
+        makeBlueViolet.addEventListener('click', getBlueViolet)
+        function getBlueViolet(){
+            document.body.style.backgroundColor = 'BlueViolet';
+        }
